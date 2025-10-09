905e15bfe9db2525ebc3a4c4a136ba14548e92d2
IegoCosta <iegosergio1320@gmail.com>
2025-10-08 22:53:30 -0400

    ✅ test: adicionar testes automatizados para título e nome do aluno

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verifica se o título principal é renderizado corretamente', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titulo = screen.getByText(/Projeto DevOps - CI\/CD Demo/i);
+  expect(titulo).toBeInTheDocument();
+});
+
+test('verifica se o nome do aluno está presente', () => {
+  render(<App />);
+  const nomeAluno = screen.getByText(/Iego Sérgio/i);
+  expect(nomeAluno).toBeInTheDocument();
 });
